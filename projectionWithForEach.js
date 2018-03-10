02a7577b182c3c5056a7c1b1c48cdb8d53b9047b
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2018-03-10 13:05:28 -0800

    Projection array in to another with forEach

---
+++ projectionWithForEach.js
@@ -0,0 +1,49 @@
+// To project one array into another, we apply a projection function to each item in the array
+// and collect the results in a new array.
+
+function arrayProjection() {
+  var newReleases = [
+			{
+				"id": 70111470,
+				"title": "Die Hard",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [4.0],
+				"bookmark": []
+			},
+			{
+				"id": 654356453,
+				"title": "Bad Boys",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [5.0],
+				"bookmark": [{ id: 432534, time: 65876586 }]
+			},
+			{
+				"id": 65432445,
+				"title": "The Chamber",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [4.0],
+				"bookmark": []
+			},
+			{
+				"id": 675465,
+				"title": "Fracture",
+				"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+				"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+				"rating": [5.0],
+				"bookmark": [{ id: 432534, time: 65876586 }]
+			}
+		];
+
+  var videoIdAndTitle = [];
+
+  newReleases.forEach(function(video) {
+    videoIdAndTitle.push({ id: video.id, title: video.title });
+  });
+  
+  console.log('New Array (projection)--->', videoIdAndTitle);
+}
+
+arrayProjection();
